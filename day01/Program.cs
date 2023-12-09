241a7b5433eec68df077c16ca8eea360d8aef23d
Jason Eudy <jle77@outlook.com>
2023-12-08 22:50:04 -0500

    Day 1 and 2

---
+++ day01/Program.cs
@@ -0,0 +1,51 @@
+﻿var tokens = new Dictionary<string, int>()
+{
+    { "1", 1 },
+    { "2", 2 },
+    { "3", 3 },
+    { "4", 4 },
+    { "5", 5 },
+    { "6", 6 },
+    { "7", 7 },
+    { "8", 8 },
+    { "9", 9 },
+    { "0", 0 },
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 }
+};
+
+int calibrationSum = 0;
+
+using (var filestream = File.OpenRead("input.txt"))
+{
+    using var reader = new StreamReader(filestream);
+
+    var foundTokenValues = new List<int>();
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        foundTokenValues.Clear();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            // Determine if the substring starting at this position matches any token
+            var match = tokens.Keys.FirstOrDefault(t => line[i..].StartsWith(t));
+            if (match != null)
+            {
+                foundTokenValues.Add(tokens[match]);
+            }
+        }
+
+        var calibrationValue = foundTokenValues.First() * 10 + foundTokenValues.Last();
+        calibrationSum += calibrationValue;
+    }
+}
+
+Console.WriteLine(calibrationSum.ToString());
---
+++ day02/Program.cs
@@ -0,0 +1,77 @@
+﻿// Part 1
+// Configuration -- the cubes the bag is loaded with.
+var configuration = new Dictionary<string, int>()
+{
+    { "red", 12 },
+    { "green", 13 },
+    { "blue", 14 }
+};
+
+int gameIdSum = 0;
+
+using (var filestream = File.OpenRead("input.txt"))
+{
+    using var reader = new StreamReader(filestream);
+
+
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        // Parse the line
+        var firstSplit = line.Split(':');
+        var gameId = int.Parse(firstSplit[0][5..]);
+        var draws = firstSplit[1].Split(';');
+
+        // Parse the draws
+        bool violation = false;
+        foreach (var draw in draws)
+        {
+            var items = draw.Split(',').Select(i => i.Trim());
+            violation = items
+                .Select(i => i.Split(' '))
+                .Select(split => (int.Parse(split[0]), split[1]))
+                .Any(d => configuration[d.Item2] < d.Item1);
+            if (violation) break;
+        }
+
+        if (violation) continue;
+
+        gameIdSum += gameId;
+    }
+}
+
+Console.WriteLine(gameIdSum);
+
+// Part 2
+
+int powerSum = 0;
+using (var filestream = File.OpenRead("input.txt"))
+{
+    using var reader = new StreamReader(filestream);
+
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        // Parse the line
+        var firstSplit = line.Split(':');
+        var gameId = int.Parse(firstSplit[0][5..]);
+        var draws = firstSplit[1].Split(';');
+
+        var power = draws
+            .SelectMany(d => d.Split(';')) // flatten all draws
+            .SelectMany(d => d.Split(',')) // flatten all items
+            .Select(i => i.Trim().Split(' '))
+            .Select(split => (int.Parse(split[0]), split[1])) // parse into tuple (#, color)
+            .GroupBy(i => i.Item2) // group by color
+            .Select(grp => new
+            {
+                grp.Key,
+                Max = grp.Max(m => m.Item1)
+            })
+            .Aggregate(1, (total, incoming) => total * incoming.Max);
+
+        powerSum += power;
+    }
+}
+
+Console.WriteLine(powerSum);
